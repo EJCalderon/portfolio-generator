015d4851ecbe51abfa7c622fe81ab531bd536792
EJCalderon <emiliojaviercalderon@gmail.com>
2022-01-15 21:07:11 -0600

    revised

---
+++ app.js
@@ -0,0 +1,18 @@
+var profileDataArgs = process.argv.slice(2);
+
+const printProfileData = profileDataArr => {
+    //This is the same as...
+    for (let i = 0; i < profileDataArr.length; i+= 1) {
+    console.log(profileDataArr[i]);
+    }
+
+console.log('==========');
+
+    //This example
+    profileDataArr.forEach((profileItem) => {
+    console.log(profileItem)
+});
+};
+
+
+printProfileData(profileDataArgs);
