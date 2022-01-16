cb412885d84bc9bb576d6e8f4328c1573c703756
EJCalderon <emiliojaviercalderon@gmail.com>
2022-01-15 21:04:30 -0600

    2.1

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ var profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);
 
 const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
+    //This is the same as...
+    for (let i = 0; i < profileDataArr.length; i+= 1) {
     console.log(profileDataArr[i]);
     }
 
 console.log('==========');
-profileDataArr.forEach((profileItem) => {
+
+    //This example
+    profileDataArr.forEach((profileItem) => {
     console.log(profileItem)
 });
 };
