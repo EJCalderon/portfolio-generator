d85b0809838f1f7efa0552cb77346615ba59409f
EJCalderon <emiliojaviercalderon@gmail.com>
2022-01-15 23:34:19 -0600

    module 2 ending

--- app.js
+++ app.js
@@ -1,46 +1,10 @@
 //Section 1
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
 const fs = require('fs');
 
+const generatePage = require('./src/page-template.js');
 
-/*const printProfileData = profileDataArr => {
-    //This is the same as...
-    for (let i = 0; i < profileDataArr.length; i+= 1) {
-    console.log(profileDataArr[i]);
-    }
+const profileDataArgs = process.argv.slice(2);
 
-console.log('==========');
-
-    //This example
-    profileDataArr.forEach((profileItem) => {
-    console.log(profileItem)
-});
-};
-
-
-printProfileData(profileDataArgs);*/
+const [name, github] = profileDataArgs;
 
 //Section 2
-const generatePage = (userName, githubName) => {
-    return `
-    <!DOCTYPE html> 
-    <html lang="en"> 
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Portfolio Demo</title>
-    </head>
-  
-    <body>
-      <h1>${name}</h1>
-      <h2><a href="https://github.com/${github}">Github</a></h2>
-    </body>
-    </html>
-    `;
-};
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
-    console.log('Portfolio complete! Checkout index.html to see the output!');
-});
